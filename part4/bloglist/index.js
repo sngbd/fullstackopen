e2539dbdf0625cd540118c8721e2795da775d8fd
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-24 22:37:41 +0700

    add config

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const config = require('./utils/config')
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -14,8 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -38,7 +38,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${config.PORT}`)
 })
