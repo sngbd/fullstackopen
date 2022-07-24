4700a197ebce54274843719a31309481d5324810
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-24 22:40:35 +0700

    add blog

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,16 +4,6 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(config.MONGODB_URI)
 
