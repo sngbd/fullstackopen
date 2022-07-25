0bca3fff16ec562606e163464de48734981d4e11
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-25 07:42:42 +0700

    finish refactoring (4.2)

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,33 +1,6 @@
-require('dotenv').config()
 const config = require('./utils/config')
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
 
-mongoose.connect(config.MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.listen(PORT, () => {
+app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
