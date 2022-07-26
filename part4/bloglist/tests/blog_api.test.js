b6c2695b892e13299e68f7f2689411058fce9c40
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-26 14:45:31 +0700

    4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose')
+const helper = require('./test_helper')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
 
-test('notes are returned as json', async () => {
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
@@ -14,6 +25,29 @@ test('unique identifier property of the blog posts is named id', async () => {
   const response = await api.get('/api/blogs').expect(200)
   
   expect(response).toBeDefined()
+})
+
+test('a valid blogs can be added', async () => {
+  const newBlog = {
+    title: 'wubbalubadubdub',
+    author: 'rick',
+    url: 'https://nevergonnagiveyouup.com',
+    likes: 6969
+  }
+  
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+  const title = blogsAtEnd.map(n => n.title)
+  expect(title).toContain(
+    'wubbalubadubdub'
+  )
 })
 
 afterAll(() => {
---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,25 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'How I do my computing',
+    author: 'Richard Stallman',
+    url: 'https://stallman.org/stallman-computing.html',
+    likes: 87
+  },
+  {
+    title: 'The Roots of Lisp',
+    author: 'Paul Graham',
+    url: 'http://www.paulgraham.com/rootsoflisp.html',
+    likes: 100
+  }
+]
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+  initialBlogs, blogsInDb
+}
