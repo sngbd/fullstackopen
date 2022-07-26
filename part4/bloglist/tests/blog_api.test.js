62b28824bc639adb0259c925a18c8eda04fbc904
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-26 14:03:10 +0700

    4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -8,6 +8,12 @@ test('notes are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs').expect(200)
+  
+  expect(response).toBeDefined()
 })
 
 afterAll(() => {
