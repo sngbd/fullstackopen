df717770fc7255119f975270087e6fedcba75d15
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-27 14:17:36 +0700

    4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,12 +7,14 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
