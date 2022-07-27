7ac4ab6e856b6bdd4fe684a51317898edeab39b2
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-07-27 09:26:07 +0700

    4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,9 +1,10 @@
+require('express-async-errors')
 const config = require('./utils/config')
 const express = require('express')
-require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
@@ -12,5 +13,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
