335a27534b4bd22f12c4f86203b2de53f299ba98
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-08-02 19:45:42 +0700

    5.17

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,9 +3,10 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const usersRouter = require('./controllers/users')
+const blogsRouter = require('./controllers/blogs')
+const testingRouter = require('./controllers/testing')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 
@@ -15,8 +16,13 @@ app.use(cors())
 app.use(express.json())
 
 app.use(middleware.tokenExtractor)
-app.use('/api/blogs', blogsRouter)
-app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/blogs', blogsRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 module.exports = app
