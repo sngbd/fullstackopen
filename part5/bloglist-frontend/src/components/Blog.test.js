c0078dfe68d5f5f1d39ce91a920c1e78069a7847
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-08-02 10:06:24 +0700

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Togglable from './Togglable'
 
 describe('<Togglable />', () => {
@@ -18,15 +19,15 @@ describe('<Togglable />', () => {
     name: 'sngbd'
   }
 
-  let container
+  let component
   beforeEach(() => {
-    container = render(
+    component = render(
       <div>
         {blog.title} {blog.author}
         <Togglable buttonLabel='view' toggleLabel='hide'>
           {blog.url}
           <br />
-          likes {blog.likes} <button >like</button>
+          likes {blog.likes} <button>like</button>
           <br />
           {blog.user.name}
           <br />
@@ -34,15 +35,23 @@ describe('<Togglable />', () => {
           <button>remove</button>}
         </Togglable>
       </div>
-    ).container
+    )
   })
 
   test('renders the blog\'s title and author but not url and likes', () => {
-
     const element = screen.getByText('React Meta')
     expect(element).toBeDefined()
 
-    const div = container.querySelector('.togglableContent')
+    const div = component.container.querySelector('.togglableContent')
     expect(div).toHaveStyle('display: none')
+  })
+
+  test('blog\'s url and likes are shown when the view button has been clicked', async () => {
+    const usr = userEvent.setup()
+    const button = screen.getAllByText('view')
+    await usr.click(button[0])
+
+    expect(component.container).toHaveTextContent('reactjs.org')
+    expect(component.container).toHaveTextContent('likes 50932')
   })
 })
