5abcb550601df5088e43b56a72fd5f20842f365b
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-08-11 18:05:24 +0700

    9.5

--- part9/bmi/index.ts
+++ part9/bmi/index.ts
@@ -1,8 +1,26 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  if (!weight || !height) {
+    res.status(400).json({
+      error: "malformatted parameters"
+    });
+  }
+  else {
+    res.json({
+      weight: weight,
+      height: height,
+      bmi: calculateBmi(height, weight)
+    });
+  }
 });
 
 const PORT = 3003;
